5f79d98896df6730beb11c49b7144d195c63be69
Jonas Claes <jonas@jonasclaes.be>
2022-01-15 11:51:43 +0100

    Improved PWA

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Discord Bot Frontend',
+    themeColor: '#3b82f6',
+    msTileColor: '#eeeeee',
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
+};
